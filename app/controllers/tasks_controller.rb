702445612268369c5bf166d0dcf00413dd1cdf1d
Charmaine Lee <charmaine@seeksophie.com>
2019-11-19 14:48:53 +0800

    show html complete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,29 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
   def show
-     @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
    end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    new_task = Task.new(task_params)
+
+    if new_task.save
+      redirect_to tasks_path(new_task)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :detail, :completed)
+  end
 end
