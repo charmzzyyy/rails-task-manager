bbb964b3c28613cd2729eb3318cfab6cf152bbf9
Charmaine Lee <charmaine@seeksophie.com>
2019-11-19 15:42:16 +0800

    edit tasks completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
-  post "tasks", to: "tasks#create"
   get "tasks/new", to: "tasks#new"
+
   get 'tasks', to: 'tasks#index'
+
   get "tasks/:id", to: "tasks#show", as: "task"
+  post "tasks", to: "tasks#create"
+
+  get    "tasks/:id/edit", to: "tasks#edit", as: "edit"
+  patch  "tasks/:id",      to: "tasks#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
